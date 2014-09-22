f3716f4d9db1265730ea861e0bb4a32b5013dd2d
Dmitry Samsonau <demn_@tut.by>
2014-09-22 15:50:21 +0300

    Initial commit

---
+++ DotNetOpenAuth/ClientProviders/Default.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientProviders {
+    public static class Default {
+        public static int Add(int a, int b) {
+            return a + b;
+        }
+    }
+}
---
+++ DotNetOpenAuth/DotNetOpenAuth.ClientProviders.Tests/DefaultTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClientProviders;
+
+namespace DotNetOpenAuth.ClientProviders.Tests {
+    [TestClass]
+    public class DefaultTests {
+        [TestMethod]
+        public void TestAddToReturnRightValue() {
+            Assert.AreEqual(Default.Add(5, 3), 8);
+        }
+
+        [TestMethod]
+        public void TestAddToReturnWrongValue() {
+            Assert.AreNotEqual(Default.Add(5, 3), 9);
+        }
+    }
+}
