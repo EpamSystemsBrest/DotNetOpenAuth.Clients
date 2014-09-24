1f899ef51bf97b0e0515f3b7510a9ce22b976c56
Dmitry Samsonau <demn_@tut.by>
2014-09-24 18:29:34 +0300

    Cleaning up the mess in FlickrOAuthClient

--- DotNetOpenAuth.Clients/FlickrOAuthClient.cs
+++ DotNetOpenAuth.Clients/FlickrOAuthClient.cs
@@ -14,11 +14,12 @@ namespace Clients {
         private const string AccessTokenUrl = "https://www.flickr.com/services/oauth/access_token";
         private const string AuthorizeUrl = "https://www.flickr.com/services/oauth/authorize";
         private const string ApiUrl = "https://api.flickr.com";
+        private const string SignatureMethod = "HMAC-SHA1";
 
         private readonly string _appId;
         private readonly string _appSecret;
-        private string _oauthToken;
         private string _tokenSecret;
+        private readonly Random _rand = new Random();
 
         public FlickrOAuthClient(string appId, string appSecret) {
             _appId = appId;
@@ -30,90 +31,91 @@ namespace Clients {
         public string ProviderName { get { return "Flickr"; } }
 
         public void RequestAuthentication(HttpContextBase context, Uri returnUrl) {
-            var rand = new Random();
-            var nonce = rand.Next(999999).ToString(CultureInfo.InvariantCulture);
+            var url = CreateRequestTokenUrl(returnUrl);
+            var request = GetRequestStringFromUrl(url);
+            var requestToken = GetValueFromRequest(request, "oauth_token");
+            _tokenSecret = GetValueFromRequest(request, "oauth_token_secret");
 
-            var parameters = "oauth_callback=" + UrlHelper.Encode(returnUrl.AbsoluteUri)
-                                + "&oauth_consumer_key=" + _appId
-                                + "&oauth_nonce=" + nonce
-                                + "&oauth_signature_method=HMAC-SHA1"
-                                + "&oauth_timestamp=" + GetTimestamp()
-                                + "&oauth_version=1.0";
-
-            var signature = GenerateSignature("GET", RequestTokenUrl, parameters);
-
-            var url = RequestTokenUrl + "?" + parameters + "&oauth_signature=" + signature;
-
-            var client = new WebClient();
-            var request = client.DownloadString(url);
-            var requestToken = request.Split(new[] { '&' }, StringSplitOptions.RemoveEmptyEntries)[1];
-            var requestTokenSecret = request.Split(new[] { '&' }, StringSplitOptions.RemoveEmptyEntries)[2];
-            _tokenSecret = requestTokenSecret.Split('=')[1];
-            var redirectUrl = AuthorizeUrl + "?" + requestToken;
-
-            HttpContext.Current.Response.Redirect(redirectUrl, false);
+            HttpContext.Current.Response.Redirect(AuthorizeUrl + "?oauth_token=" + requestToken, false);
         }
 
         public AuthenticationResult VerifyAuthentication(HttpContextBase context) {
-            var oauthVerifier = context.Request.QueryString["oauth_verifier"];
-            _oauthToken = context.Request.QueryString["oauth_token"];
-
-            var rand = new Random();
-            var nonce = rand.Next(999999).ToString(CultureInfo.InvariantCulture);
-
-            var parameters =
-                        "oauth_consumer_key=" + _appId +
-                        "&oauth_nonce=" + nonce +
-                        "&oauth_signature_method=HMAC-SHA1" +
-                        "&oauth_timestamp=" + GetTimestamp() +
-                        "&oauth_token=" + _oauthToken +
-                        "&oauth_verifier=" + oauthVerifier +
-                        "&oauth_version=1.0";
-            var sig = GenerateSignature("GET", AccessTokenUrl, parameters);
-            var url = AccessTokenUrl + "?" + parameters + "&oauth_signature=" + sig;
-            var client = new WebClient();
-            var request = client.DownloadString(url);
-            var userData = CreateUserInfo(request);
-            return new AuthenticationResult(
-                    isSuccessful: true,
-                    provider: ProviderName,
-                    providerUserId: userData.user_nsid,
-                    userName: userData.username,
-                    extraData:
-                        new Dictionary<string, string>
-                        {
-                            {"FullName", userData.fullname},
-                        });
-        }
-
-        private UserData CreateUserInfo(string request) {
-            return new UserData {
-                fullname = HttpUtility.ParseQueryString(request).Get("fullname"),
-                user_nsid = HttpUtility.ParseQueryString(request).Get("user_nsid"),
-                username = HttpUtility.ParseQueryString(request).Get("username")
-            };
+            var url = CreateUserInfoUrl(context);
+            var request = GetRequestStringFromUrl(url);
+            var userData = UserData.CreateUserInfo(request);
+            return CreateAuthenticationResult(userData);
         }
 
         #endregion IAuthenticationClient
 
-        private string GenerateSignature(string httpMethod, string apiEndpoint, string parameters) {
-            var basestring = httpMethod + "&" + UrlHelper.Encode(apiEndpoint) + "&" + UrlHelper.Encode(parameters);
+        private string CreateRequestTokenUrl(Uri returnUrl) {
+            var parameters = "oauth_callback=" + Encode(returnUrl.AbsoluteUri) +
+                             "&oauth_consumer_key=" + _appId +
+                             "&oauth_nonce=" + GenerateNonce() +
+                             "&oauth_signature_method=" + SignatureMethod +
+                             "&oauth_timestamp=" + GetTimestamp() +
+                             "&oauth_version=1.0";
+
+            var signature = GenerateSignature("GET", RequestTokenUrl, parameters, true);
+            var url = RequestTokenUrl + "?" + parameters + "&oauth_signature=" + signature;
+            return url;
+        }
+
+        private static string GetRequestStringFromUrl(string url) {
+            var client = new WebClient();
+            var request = client.DownloadString(url);
+            return request;
+        }
+
+        private static string GetValueFromRequest(string request, string value) {
+            return HttpUtility.ParseQueryString(request).Get(value);
+        }
+
+        private AuthenticationResult CreateAuthenticationResult(UserData userData) {
+            return new AuthenticationResult(
+                isSuccessful: true,
+                provider: ProviderName,
+                providerUserId: userData.UserNsid,
+                userName: userData.Username,
+                extraData:
+                    new Dictionary<string, string>
+                    {
+                        {"FullName", userData.Fullname},
+                    });
+        }
+
+        private string CreateUserInfoUrl(HttpContextBase context) {
+            var oauthVerifier = context.Request.QueryString["oauth_verifier"];
+            var oauthToken = context.Request.QueryString["oauth_token"];
+
+            var parameters = "oauth_consumer_key=" + _appId +
+                             "&oauth_nonce=" + GenerateNonce() +
+                             "&oauth_signature_method=HMAC-SHA1" +
+                             "&oauth_timestamp=" + GetTimestamp() +
+                             "&oauth_token=" + oauthToken +
+                             "&oauth_verifier=" + oauthVerifier +
+                             "&oauth_version=1.0";
+
+            var sig = GenerateSignature("GET", AccessTokenUrl, parameters);
+            var url = AccessTokenUrl + "?" + parameters + "&oauth_signature=" + sig;
+            return url;
+        }
+
+        private string GenerateSignature(string httpMethod, string apiEndpoint, string parameters, bool getToken = false) {
+            var basestring = httpMethod + "&" + Encode(apiEndpoint) + "&" + Encode(parameters);
 
             var encoding = new ASCIIEncoding();
 
-            //create key (request_token can be an empty string)
-            var key = _appSecret + "&" + _tokenSecret;
+            var key = getToken ? _appSecret + "&" : _appSecret + "&" + _tokenSecret;
             var keyByte = encoding.GetBytes(key);
 
-            //create message to encrypt
             var messageBytes = encoding.GetBytes(basestring);
             string signature;
-            //encrypt message using hmac-sha1 with the provided key
             using (var hmacsha1 = new HMACSHA1(keyByte)) {
                 byte[] hashmessage = hmacsha1.ComputeHash(messageBytes);
                 signature = Convert.ToBase64String(hashmessage);
             }
-            return UrlHelper.Encode(signature);
+            return Encode(signature);
         }
 
         private static string GetTimestamp() {
@@ -121,21 +123,25 @@ namespace Clients {
                 ToString(CultureInfo.InvariantCulture);
         }
 
-        private class UserData {
-            public string fullname;
-            public string user_nsid;
-            public string username;
+        private string GenerateNonce() {
+            return _rand.Next(999999).ToString(CultureInfo.InvariantCulture);
         }
-    }
 
-    /// <summary> 
-    /// URL encoding class.  Note: use at your own risk. 
-    /// Written by: Ian Hopkins (http://www.lucidhelix.com) 
-    /// Date: 2008-Dec-23 
-    /// (Ported to C# by t3rse (http://www.t3rse.com)) 
-    /// </summary> 
-    public static class UrlHelper {
-        public static string Encode(string str) {
+        private class UserData {
+            public string Fullname;
+            public string UserNsid;
+            public string Username;
+
+            public static UserData CreateUserInfo(string queryString) {
+                return new UserData {
+                    Fullname = HttpUtility.ParseQueryString(queryString).Get("fullname"),
+                    UserNsid = HttpUtility.ParseQueryString(queryString).Get("user_nsid"),
+                    Username = HttpUtility.ParseQueryString(queryString).Get("username")
+                };
+            }
+        }
+
+        private static string Encode(string str) {
             var charClass = String.Format("0-9a-zA-Z{0}", Regex.Escape("-_.!~*'()"));
             return Regex.Replace(str, String.Format("[^{0}]", charClass), EncodeEvaluator);
         }
