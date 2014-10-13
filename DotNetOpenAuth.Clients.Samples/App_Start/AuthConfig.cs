53d18bfc24bf397f11f50c09f7a26cc0565c7328
Dmitry Samsonau <demn_@tut.by>
2014-10-13 17:36:26 +0300

    Refactoring over tumblr and twitter *Commit from VS

--- DotNetOpenAuth.Clients.Samples/App_Start/AuthConfig.cs
+++ DotNetOpenAuth.Clients.Samples/App_Start/AuthConfig.cs
@@ -1,9 +1,12 @@
 ﻿using DotNetOpenAuth.AspNet.Clients;
 using Microsoft.Web.WebPages.OAuth;
 
-namespace DotNetOpenAuth.Clients.Samples {
-    public class AuthConfig {
-        public static void RegisterAuth() {
+namespace DotNetOpenAuth.Clients.Samples
+{
+    public class AuthConfig
+    {
+        public static void RegisterAuth()
+        {
             // To let users of this site log in using their accounts from other sites such as Microsoft, Facebook, and Twitter,
             // you must update this site. For more information visit http://go.microsoft.com/fwlink/?LinkID=252166
 
@@ -23,7 +26,6 @@ namespace DotNetOpenAuth.Clients.Samples {
 
             OAuthWebSecurity.RegisterClient(new FlickrOAuthClient("c2b9248adf2b3a90214a1c20fe1fdef6", "c1c66b0a7f1ca84d"));
             OAuthWebSecurity.RegisterClient(new GooglePlusOAuthClient("1067161712157-p1969b3q6q18v0c2anp97iarthtrmis2.apps.googleusercontent.com", "VE2aut2r8nKrmoYGGjLyrilt"));
-            OAuthWebSecurity.RegisterClient(new PinterestOAuthClient("", ""));
             OAuthWebSecurity.RegisterClient(new TumblrOAuthClient("fq4O66FfwrEoXISW8xKaeGh8LOQYBHV8IBH9pPTkRPbxv8GW0M", "baHrYQazVReM0CDomuTljbo5yNA8ZvAejxovMLiMoak78AF0jk"));
             OAuthWebSecurity.RegisterClient(new TwitterOAuthClient("lFieIqoyzN6lvDqQPizYeGh2y", "WdPOrgATGvdDN1t3VZxu36EjqLfNpX4zhVAeiFJnWyI2VMRGCH"));
             OAuthWebSecurity.RegisterClient(new VkOAuthClient("4559228", "pkzqWBIXivRKrN8esLTS"));
